ca29dd6716a3c3127b8359d3e4456867e40d6bc6
AdityaMeena <117091006+Aditya-meena-tw@users.noreply.github.com>
2023-05-17 18:06:43 +0530

    Update unit-test.py

--- unit-test.py
+++ unit-test.py
@@ -1,32 +1,38 @@
 import random
+import unittest
 
 def generate_random_numbers():
-return [random.randrange(21) for _ in range(5)]
+    return [random.randrange(21) for _ in range(5)]
 
 def get_user_inputs():
-return [int(input('Enter number: ')) for _ in range(5)]
+    return [int(input('Enter number: ')) for _ in range(5)]
 
 def calculate_sum(numbers):
-return sum(numbers)
+    return sum(numbers)
 
 def determine_winner(sum1, sum2):
-if sum1 > sum2:
-return 'Player 1 is the winner'
-else:
-return 'Player 2 is the winner'
+    if sum1 > sum2:
+        return 'Player 1 is the winner'
+    else:
+        return 'Player 2 is the winner'
 
-#Generate random numbers for player 1
-list1 = generate_random_numbers()
-print(list1)
-sum1 = calculate_sum(list1)
-print(sum1)
+class MyTestCase(unittest.TestCase):
+    def test_game(self):
+        # Generate random numbers for player 1
+        list1 = generate_random_numbers()
+        self.assertEqual(len(list1), 5)
+        sum1 = calculate_sum(list1)
+        self.assertEqual(sum1, sum(list1))
 
-#Get user inputs for player 2
-list2 = get_user_inputs()
-print(list2)
-sum2 = calculate_sum(list2)
-print(sum2)
+        # Get user inputs for player 2
+        list2 = get_user_inputs()
+        self.assertEqual(len(list2), 5)
+        sum2 = calculate_sum(list2)
+        self.assertEqual(sum2, sum(list2))
 
-#Determine the winner
-winner = determine_winner(sum1, sum2)
-print(winner)
+        # Determine the winner
+        winner = determine_winner(sum1, sum2)
+        self.assertIn(winner, ['Player 1 is the winner', 'Player 2 is the winner'])
+
+if __name__ == '__main__':
+    unittest.main()
