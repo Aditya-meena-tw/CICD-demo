94908be8e6dd29b4e915e108064ddaf4ca926600
AdityaMeena <117091006+Aditya-meena-tw@users.noreply.github.com>
2023-05-22 12:05:38 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,16 +1,24 @@
+import random
 import unittest
 
 def generate_random_numbers():
     # Code for generating random numbers
+    return [random.randrange(21) for _ in range(5)]
 
 def get_user_inputs():
     # Code for getting user inputs
+    return [int(input('Enter number: ')) for _ in range(5)]
 
 def calculate_sum(numbers):
     # Code for calculating the sum of numbers
+    return sum(numbers)
 
 def determine_winner(sum1, sum2):
     # Code for determining the winner
+    if sum1 > sum2:
+        return 'Player 1 is the winner'
+    else:
+        return 'Player 2 is the winner'
 
 class MyTestCase(unittest.TestCase):
     def test_game(self):
