3cfb7f501040bef22b0aa8ff20405db0953a0fe8
AdityaMeena <117091006+Aditya-meena-tw@users.noreply.github.com>
2023-05-22 12:03:17 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,20 +1,16 @@
-import random
 import unittest
 
 def generate_random_numbers():
-    return [random.randrange(21) for _ in range(5)]
+    # Code for generating random numbers
 
 def get_user_inputs():
-    return [int(input('Enter number: ')) for _ in range(5)]
+    # Code for getting user inputs
 
 def calculate_sum(numbers):
-    return sum(numbers)
+    # Code for calculating the sum of numbers
 
 def determine_winner(sum1, sum2):
-    if sum1 > sum2:
-        return 'Player 1 is the winner'
-    else:
-        return 'Player 2 is the winner'
+    # Code for determining the winner
 
 class MyTestCase(unittest.TestCase):
     def test_game(self):
@@ -24,8 +20,8 @@ class MyTestCase(unittest.TestCase):
         sum1 = calculate_sum(list1)
         self.assertEqual(sum1, sum(list1))
 
-        # Get user inputs for player 2
-        list2 = get_user_inputs()
+        # Provide predefined inputs for player 2
+        list2 = [1, 2, 3, 4, 5]
         self.assertEqual(len(list2), 5)
         sum2 = calculate_sum(list2)
         self.assertEqual(sum2, sum(list2))
